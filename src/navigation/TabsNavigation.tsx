eef987b7a844fd08ca85b01367c67d455d8cb777
leedd87 <leedav87@gmail.com>
2023-09-14 21:01:39 -0300

    add icons

--- src/navigation/TabsNavigation.tsx
+++ src/navigation/TabsNavigation.tsx
@@ -2,20 +2,41 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from '../screens/HomeScreen';
 import { FavoritesScreen } from '../screens/FavoritesScreen';
-import { DetailScreen } from '../screens/DetailScreen';
 import { RootStackParams } from './Navigation';
+import { Text } from 'react-native';
+
 
 const Tab = createBottomTabNavigator<RootStackParams>();
 
 export const TabsNavigator = () => {
     return (
         <Tab.Navigator
-            screenOptions={{
+            screenOptions={({ route }) => ({
                 headerShown: false,
-            }}
+                tabBarActiveTintColor: 'blue',
+                tabBarStyle: {
+                    borderTopColor: 'blue',
+                    borderTopWidth: 1
+                },
+                tabBarLabelStyle: {
+                    fontSize: 12
+                },
+                tabBarIcon: ({ color, focused, size }) => {
+                    let iconName: string = '';
+                    switch (route.name) {
+                        case 'Home':
+                            iconName = 'T1'
+                            break;
+                        case 'Favorites':
+                            iconName = 'T2'
+                            break;
+                    }
+                    return <Text style={{ color: 'red' }}>{iconName}</Text>
+                }
+            })}
         >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Favorites" component={FavoritesScreen} />
+            <Tab.Screen name="Home" options={{ title: 'Home' }} component={HomeScreen} />
+            <Tab.Screen name="Favorites" options={{ title: 'Favorites' }} component={FavoritesScreen} />
         </Tab.Navigator>
     );
 }
