dda4c75f8447c24e1a6dc300116981409f0ccd0c
leedd87 <leedav87@gmail.com>
2023-11-06 09:19:08 -0300

    syntax error fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { newsSlice } from '../redux/newsSlices'
-
+import {configureStore} from '@reduxjs/toolkit';
+import {newsSlice} from '../redux/newsSlices';
 
 export const store = configureStore({
     reducer: {
-        newsReducer: newsSlice.reducer
+        newsReducer: newsSlice.reducer,
     },
-})
+});
 
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
